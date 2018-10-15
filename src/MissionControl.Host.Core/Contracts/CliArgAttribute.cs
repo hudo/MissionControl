a976892341580d38bd48c2250de13d6665c00a29
Hrvoje <hrvoje.hudoletnjak@gmail.com>
2018-10-15 17:34:11 +0100

    missing file ... :(

---
+++ src/MissionControl.Host.Core/Contracts/CliArgAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MissionControl.Host.Core.Contracts
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class CliArgAttribute : Attribute
+    {
+        public CliArgAttribute(bool required = false, string help = null)
+        {
+            Required = required;
+            Help = help;
+        }
+        
+        public bool Required { get; }
+        public string Help { get; }
+    }
+}
